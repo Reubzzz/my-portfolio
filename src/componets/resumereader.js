6dbc800fa9c93c1af09cf3f169f4b2b5891845a5
Reubzzz <98785049+Reubzzz@users.noreply.github.com>
2022-10-09 15:33:25 +1300

    responsive update cv

--- src/componets/resumereader.js
+++ src/componets/resumereader.js
@@ -29,7 +29,7 @@ class ResumePage extends Component {
             <div id="ResumeContainer">
                 <Document className={"PDFDocument"} file={file} onLoadSuccess={this.onDocumentLoadSuccess}>
                     <Page className={"PDFPage PDFPageOne"} pageNumber={pageNumber} renderTextLayer={false} renderInteractiveForms={false} />
-                    <Page className={"PDFPage"} pageNumber={pageNumber + 1} renderTextLayer={false} renderInteractiveForms={false} />
+                    <Page className={"PDFPage PDFPageTwo"} pageNumber={pageNumber + 1} renderTextLayer={false} renderInteractiveForms={false} />
                     <Page className={"PDFPage"} pageNumber={pageNumber + 2} renderTextLayer={false} renderInteractiveForms={false} />
                 </Document>
             </div>
